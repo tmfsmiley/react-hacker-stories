8fd5ba34337d0105e7d046a2af1c87998c24a124
Christine Noonan <cnoonan@fool.com>
2023-06-09 12:41:12 -0400

    working with lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,55 @@
 import * as React from 'react';
 
-const title = 'React';
+// function getTitle(title) {
+//   return title;
+// }
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="search">Search:
+        <input id="search" type="text" />
+      </label>
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
