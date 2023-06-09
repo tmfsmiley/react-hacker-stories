218da96b8f553291c92848302e336c79c6323613
Christine Noonan <cnoonan@fool.com>
2023-06-09 14:21:32 -0400

    the start of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
 
-// function getTitle(title) {
-//   return title;
-// }
-
 const list = [
   {
     title: 'React',
@@ -23,24 +19,13 @@ const list = [
   },
 ];
 
-function App() {
-
+function List() {
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search:
-        <input id="search" type="text" />
-      </label>
-
-      <hr />
-
-      <ul>
+    <ul>
         {list.map(function (item) {
           return (
             <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
+              <span><a href={item.url}>{item.title}</a></span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
@@ -48,6 +33,28 @@ function App() {
           );
         })}
       </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:
+        <input id="search" type="text" />
+      </label>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+
+      <hr />
+
+      <List />
     </div>
   );
 }
