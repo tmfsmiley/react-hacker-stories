6cc9c0376216de94e5cad7311f413deab3445d3f
Christine Noonan <cnoonan@fool.com>
2023-06-30 12:30:48 -0400

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -48,11 +48,17 @@ const App = () => {
   );
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
+
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false);
+    })
+    .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
@@ -73,6 +79,7 @@ const App = () => {
 
   return (
     <div>
+
       <h1>My Hacker Stories</h1>
 
       <InputWithLabel
@@ -85,7 +92,17 @@ const App = () => {
       </InputWithLabel>
 
       <hr />
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+
+        {isError && <p>Something went wrong...</p>}
+
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <List
+            list={searchedStories}
+            onRemoveItem={handleRemoveStory}
+          />
+        )}
     </div>
   );
 };
