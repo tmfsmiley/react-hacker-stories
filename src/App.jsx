c42bce1aad354ab916f135c0752c2190891f50e1
Christine Noonan <cnoonan@fool.com>
2023-06-30 11:06:21 -0400

    asynchronous data

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,14 @@ const initialStories = [
   },
 ];
 
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(
+      () => resolve({ data: {stories: initialStories } }),
+      2000
+    )
+  );
+
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState(
@@ -39,7 +47,13 @@ const App = () => {
     'React'
   );
 
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = (item) => {
     const newStories = stories.filter(
