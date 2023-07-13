2ad07b96f16dd32bb92146e741d2d9a5af5a09c2
Christine Noonan <cnoonan@fool.com>
2023-07-13 10:30:52 -0400

    css additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import axios from 'axios';
+import './App.css';
 // import PropTypes from 'prop-types';
 
 const storiesReducer = (state, action) => {
@@ -102,16 +103,14 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-
-      <hr />
 
         {stories.isError && <p>Something went wrong...</p>}
 
@@ -129,7 +128,10 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form
+    onSubmit={onSearchSubmit}
+    className="search-form"
+  >
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -139,7 +141,11 @@ const SearchForm = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
@@ -163,13 +169,14 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}&nbsp;
+      <label htmlFor={id} className="label">{children}&nbsp;
         <input
           ref={inputRef}
           id={id}
           type={type}
           value={value}
           onChange={onInputChange}
+          className="input"
         />
       </label>
     </>
@@ -189,13 +196,19 @@ const List = ({ list, onRemoveItem }) => (
 );
 
 const Item = ({ item, onRemoveItem }) => (
-  <li>
-    <span><a href={item.url}>{item.title}</a></span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>Dismiss</button>
+  <li className="item">
+    <span style={{ width: '40%' }}><a href={item.url}>{item.title}</a></span>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)}
+        className="button button_small"
+      >
+        Dismiss
+      </button>
     </span>
   </li>
 );
